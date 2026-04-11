568da2c7a8406c20a96a95238dfd8520dd086089
AnshvermaCoding <164167471+AnshvermaCoding@users.noreply.github.com>
2026-04-12 00:34:37 +0530

    Update flyBirdGameJs.js

--- flyBirdGameJs.js
+++ flyBirdGameJs.js
@@ -159,6 +159,7 @@ if(localStorage.getItem("save") === "true") {
 function startGame() {
     prevScore = localStorage.getItem("score2");
     window.location.href = "FlyBird.html";
+    location.reload();
 }
 shopBlock.style.transform = "scale(0.5)";
 optionBlock.style.transform = "scale(0.5)";
